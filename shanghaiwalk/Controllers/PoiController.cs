060010a8c423d42b2ca98ac71079dc0588569f5f
yuzukwok <yuzukwok@gmail.com>
2017-06-17 15:25:32 +0800

    新增输入界面

---
+++ shanghaiwalk/Controllers/PoiController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace shanghaiwalk.Controllers
+{
+    public class PoiController: Controller
+    {
+        public IActionResult NewPoi()
+        {
+            return View();
+        }
+    }
+}
